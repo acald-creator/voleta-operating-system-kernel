a72d7bdd4166c90c68bf102e44ea4b34f66a3d3f
Antonette Caldwell <aacaldwell42@outlook.com>
2021-06-13 17:43:51 -0500

    Created the base for kernel

---
+++ voletakernellib/kernel/include/tty.h
@@ -0,0 +1,14 @@
+#ifndef VOLETA_KERNEL_TTY_H
+#define VOLETA_KERNEL_TTY_H
+
+#include <stddef.h>
+
+void terminal_initialize(void);
+
+void terminal_putchar(char c);
+
+void terminal_write(const char *data, size_t size);
+
+void terminal_writestring(const char *data);
+
+#endif //VOLETA_KERNEL_TTY_H
---
+++ voletakernellib/kernel/kernel.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+void kernel_main(void) {
+    terminal_initialize();
+    printf("Hello, kernel World!\n");
+}
